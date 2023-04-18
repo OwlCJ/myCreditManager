035afd7f4abec32919b98e6a8de62f5394f2de28
OwlCJ <pcj0308@gmail.com>
2023-04-19 05:26:23 +0900

    Add: 성적 추가 기능 추가

--- MyCreditManager/Student.swift
+++ MyCreditManager/Student.swift
@@ -5,19 +5,5 @@ struct Student {
     let name: String
     var subjectScore: [String: String] = [:]
     
-//    var scoreAverage: Int {
-//        subjectScore.values 
-//    }
+    var scoreAverage: Double = 0.0
 }
-
-//case "A+":
-//case "A":
-//case "B+":
-//case "B":
-//case "C+":
-//case "C":
-//case "D+":
-//case "D":
-//case "F":
-
-
--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -2,6 +2,7 @@
 import Foundation
 
 let functions: [String] = ["학생추가", "학생삭제", "성적추가(변경)", "성적삭제", "평점보기"]
+let scores: [String: Double] = ["A+": 4.5, "A": 4.0, "B+": 3.5, "B": 3.0, "C+": 2.5, "C": 2.0, "D+": 1.5, "D": 1.0, "F": 0]
 var students: [Student] = []
 
 while true {
@@ -48,6 +49,7 @@ func scoreManaging(_ userFunction: String?) -> Bool {
         students.append(addStudent)
         
     case "2":
+        //삭제 처리 확인을 위한 bool 변수
         var hasStudent = false
         print("삭제할 학생의 이름을 입력해주세요")
         let removeStudent = Student(name: readLine()!)
@@ -66,6 +68,43 @@ func scoreManaging(_ userFunction: String?) -> Bool {
         }
         
         if hasStudent == false { print("\(removeStudent.name)학생을 찾지 못했습니다.") }
+        
+    case "3":
+        print("""
+        성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.
+        입력예) Mickey Swift A+
+        만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.
+        """)
+        let userInput = readLine()!
+        let scoreAdding = userInput.components(separatedBy: " ")
+
+        //무입력, 띄어쓰기로 구분된 이름, 과목, 성적 3칸이 모두 입력되었는지 확인
+        guard scoreAdding.count == 3, students.contains(where: { student in
+            student.name == scoreAdding[0]
+        }) else {
+            print("입력이 잘못되었습니다. 다시 확인해주세요.")
+            break
+        }
+        
+        guard scores.keys.contains(scoreAdding[2]) else {
+            print("유효한 성적을 기입해주세요.")
+            break
+        }
+        
+        //이름을 바탕으로 students배열내에 해당 이름이 있는지 재판단후 성적추가
+        for (idx, student) in students.enumerated() {
+            if student.name == scoreAdding[0] {
+                students[idx].subjectScore.updateValue(scoreAdding[2], forKey: scoreAdding[1])
+                print("\(scoreAdding[0])학생의 \(scoreAdding[1])과목이 \(scoreAdding[2])로 추가(변경)되었습니다.")
+                
+                //scoreAverage 재연산 후 저장
+                let allScores = students[idx].subjectScore.values
+                let stringToScore = allScores.map { scores[$0]! }
+                
+                students[idx].scoreAverage = stringToScore.reduce(0.0, +) / Double(stringToScore.count)
+            }
+        }
+        
         
         
         
